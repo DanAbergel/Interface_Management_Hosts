498dd2a002a71f2d6ca764090a2cdcccc823e508
Dan Abergel <dabergel@g.jct.ac.il>
2019-12-26 15:23:37 +0200

    dan abergel

---
+++ BE/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class Configuration
+    {
+        public static int counterBankAccount;
+        public static int counterGuestRequest;
+        public static int counterHost;
+        public static int counterHostingUnit;
+        public static int counterOrder;
+    }
+}
