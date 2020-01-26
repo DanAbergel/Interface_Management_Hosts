e68fb8ee3a936d7f65d74df9eb83a05930155aa1
nathanebar <57418604+nathanebar@users.noreply.github.com>
2020-01-26 23:20:46 +0200

    nathane

--- DAL/IDAL.cs
+++ DAL/IDAL.cs
@@ -12,40 +12,44 @@ namespace DAL
         //fonctions logement    
 
         void addHostingUnit(HostingUnit hostingUnit);
-        void deleteHostingUnit(long HostingUnitKey);
+        void deleteHostingUnit(string HostingUnitKey);
         void uptadeHostingUnit(HostingUnit updateHostingUnit);
-        List<HostingUnit> getAllHostingUnits();
         List<HostingUnit> getHostingUnits(Func<HostingUnit, bool> predicate = null);
+        //List<HostingUnit> getAllHostingUnits();
+        IEnumerable<BE.HostingUnit> getAllHostingUnits(Func<BE.HostingUnit, bool> predicate = null);
+       
 
         //fonctions requete
 
         void addGuestRequest(GuestRequest guestRequest);
         void deleteGuestRequest(long guestRequestKey);
         void updateGuestRequest(GuestRequest updateGuestRequest);
-        List<GuestRequest> getAllGuestRequest();
-   
+        IEnumerable<BE.GuestRequest> getAllGuestRequest(Func<BE.GuestRequest, bool> predicate = null);
+
         //fonctions commande en cours
-      
+
         void addOrder(Order order);
         void updateOrder(Order updateOrder);
-        List<Order> getAllOrders();
-       
+        void deleteOrder(string orderKey);
+        IEnumerable<BE.Order> getAllOrders(Func<BE.Order, bool> predicate = null);
+
         //liste des comptes bancaires
 
-        List<BankBranch> GetAllBankAccounts();
+        IEnumerable<BE.BankBranch> GetAllBankAccounts(Func<BE.BankBranch, bool> predicate = null);
 
         //liste des proprietaires
 
-        List<Host> getAllHost();
+        IEnumerable<BE.Host> getAllHost(Func<BE.Host, bool> predicate = null);
 
         // fonction pour exceptions
 
-        bool HostingUnitExist(long HostingUnitKey);
+        bool HostingUnitExist(string HostingUnitKey);
         bool GuestRequestExist(long guestRequestKey);
-        bool OrderExist(long HostingUnitKey);
+        bool OrderExist(string OrderKey);
 
-        HostingUnit GetHostingUnitByID(long ID);
+        HostingUnit GetHostingUnitByID(string HostingUnitKey);
         GuestRequest GetGuestRequestByID(long ID);
+        Order GetOrderByID(string OrderKey);
 
 
     }
